aadcd23ef7b9c8ca3b9b723ea29c95417d16ec45
imdrflogger <110408498+imdrflogger@users.noreply.github.com>
2022-08-05 14:44:34 +0400

    Update models.py

--- drf_api_logger/models.py
+++ drf_api_logger/models.py
@@ -30,8 +30,8 @@ if database_log_enabled():
         status_code = models.PositiveSmallIntegerField(help_text='Response status code', db_index=True)
         execution_time = models.DecimalField(decimal_places=5, max_digits=8,
                                              help_text='Server execution time (Not complete response time.)')
-        created_at = models.DateTimeField()
-        updated_at = models.DateTimeField()
+        created_at = models.DateTimeField(null=True, blank=True)
+        updated_at = models.DateTimeField(null=True, blank=True)
 
         def __str__(self):
             return self.api
